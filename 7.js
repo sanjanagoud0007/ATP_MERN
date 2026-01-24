697e889928c45b7258eb9455a918f32bf16ddee2
sanjana <sanjanagoud0007@gmail.com>
2026-01-24 17:38:43 +0530

    Restructured: only tracking assignments folder

--- 7.js
+++
@@ -1,7 +0,0 @@
-//mutable(all ref types are mutable)
-
-//immutable(all primitives are immutable)
-let a=10;
-a=a+1
-a=a*10
-console.log("a is ",a)
--- assignment_2/todo_app_module/app.js
+++
@@ -1,5 +0,0 @@
-import { addTask, getAllTasks, completeTask } from './task.js';
-console.log(addTask("Study", "high", "2026-02-01"));
-console.log(getAllTasks());
-console.log(completeTask(1));
-console.log(getAllTasks());
